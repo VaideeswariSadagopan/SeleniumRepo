46b5a992ed233df493d56b2792adda85c912a9fd
VaideeswariSadagopan <svaideeswari@gmail.com>
2024-11-04 12:07:52 +0300

    Add files via upload
    week2.day1

---
+++ day1/LaunchBrowser.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    ChromeDriver driver= new ChromeDriver();
+    EdgeDriver driver2= new EdgeDriver();
+    FirefoxDriver driver3= new FirefoxDriver();
+    
+	}
+
+}
